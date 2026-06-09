94d5da0d5b9d6ea41973ce9891fa183d731241bf
Karlateshima <karla.teshima.2012@gmail.com>
2026-06-09 13:44:41 +0900

    fix(backend): remove timezone usage that broke build

--- test_axios.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-const client = axios.create({ baseURL: 'http://localhost:3000' });
-client.interceptors.request.use(config => {
-  config.headers.Authorization = 'Bearer TEST_TOKEN';
-  return config;
-});
-console.log(client.interceptors);
